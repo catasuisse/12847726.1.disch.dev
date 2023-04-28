70718e1cd19d6264d8591d27879115b9743192f4
Maik <catasuisse@icloud.com>
2023-04-28 10:12:25 +0200

    –

--- theme/template-single.php
+++ theme/template-single.php
@@ -2,7 +2,7 @@
 <html lang="<?php echo rex_clang::getCurrent()->getCode(); ?>">
     <?php require_once('partials/head.php'); ?>
 
-    <body class="dd-loading-state-1 dd-loading-state-2 -dd-scroll-t-05 dd-template-single<?php echo $navIsHidden ? ' dd-nav-is-hidden' : null; ?>">
+    <body class="dd-loading-state-1 dd-loading-state-2 -dd-scroll-t-05 dd-template-single">
         <div class="dd-main-wrapper">
             <main data-scroll-container>
                 <?php
